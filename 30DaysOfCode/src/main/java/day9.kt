8f3c4f77bb715baee68ab3a08bc136e76a7aae19
jadeflower968 <jadeflower968@gmail.com>
2018-03-16 13:07:15 +0000

    day9

---
+++ 30DaysOfCode/src/main/java/day9.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+    val n = reader.nextInt()
+
+    println(factorial(n))
+    reader.close()
+}
+
+fun factorial(N: Int): Int {
+    if (N <= 1) {
+        return 1
+    } else {
+        return N * factorial(N-1)
+    }
+}
